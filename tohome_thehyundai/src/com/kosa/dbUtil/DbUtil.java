f13912e722e78bf7b7bb53557fe4033c156d88ae
bansongs <predicthappy@naver.com>
2022-09-06 10:12:11 +0900

    dbutil 소스코드

--- tohome_thehyundai/src/com/kosa/dbUtil/DbUtil.java
+++ tohome_thehyundai/src/com/kosa/dbUtil/DbUtil.java
@@ -1,5 +1,14 @@
-package com.kosa.dbUtil;
+ package com.kosa.dbUtil;
 
-public class util {
-
-}
+  public class DbUtil {
+//	  public static String driver = "oracle.jdbc.driver.OracleDriver";
+//	  public static String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+//	  public static String user = "ace";
+//	  public static String password = "me";
+//	  
+	  public static String driver = "oracle.jdbc.driver.OracleDriver";
+      public static String url = "jdbc:oracle:thin:@edudb_high?TNS_ADMIN=C:\\\\Dev\\\\OracleWallet\\\\Wallet_edudb";
+	  public static String user = "minseok";
+	  public static String password = "xxxxxxAt21cc";
+	  
+  }

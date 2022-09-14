2ad6000ba100bc815e22720bff68bf21dd6d704f
kosa <kosa@DESKTOP-I92AVJE>
2022-09-14 11:38:33 +0900

    giwon 주문 내역 조회 기능 생성 -orderDAO, orderVO 수정

--- tohome_thehyundai/src/com/kosa/entity/OrdersVO.java
+++ tohome_thehyundai/src/com/kosa/entity/OrdersVO.java
@@ -1,5 +1,18 @@
 package com.kosa.entity;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import lombok.Data;
+
+@Data
 public class OrdersVO {
 
+	private int orderId;
+	private Date orderDate;
+	private String paymenttype;
+	private int totalPrice;
+	private String orderState;
+	private int orderCount;
 }

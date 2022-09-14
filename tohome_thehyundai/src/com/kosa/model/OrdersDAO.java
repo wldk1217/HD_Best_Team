9ff80e47471d72e2c798a09d29c2d78e68c60816
kosa <kosa@DESKTOP-I92AVJE>
2022-09-14 16:33:48 +0900

    giwon 주문 내역 상세보기 기능 추가 -OrderDAO, OrderVO 수정

--- tohome_thehyundai/src/com/kosa/model/OrdersDAO.java
+++ tohome_thehyundai/src/com/kosa/model/OrdersDAO.java
@@ -8,7 +8,9 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import com.kosa.dbUtil.DBConnection;
+import com.kosa.entity.OrderListVO;
 import com.kosa.entity.OrdersVO;
+import com.kosa.entity.ProductVO;
 
 import oracle.jdbc.OracleTypes;
 
@@ -25,11 +27,14 @@ public class OrdersDAO {
 	
 	
 	/* 주문 테스트시 사용 */
-	public static void main(String[] args) {
-		OrdersDAO dao = OrdersDAO.getInstance();
-		ArrayList<OrdersVO> arr = dao.listOrder("admin12");
-		System.out.println(arr);
-	}
+//	public static void main(String[] args) {
+//		OrdersDAO dao = OrdersDAO.getInstance();
+//		ArrayList<OrdersVO> arr = dao.listOrder("admin12");
+//		System.out.println(arr);
+//		ArrayList<OrderListVO> arr = dao.orderListDetail(1);
+//		System.out.println(arr);
+//		
+//	}
 	
 	//주문 내역 조회
 	public ArrayList<OrdersVO> listOrder(String memberID) {
@@ -70,5 +75,45 @@ public class OrdersDAO {
 		}
 		return orderList;
 
+	}
+	
+	//주문 상세 정보
+	public ArrayList<OrderListVO> orderListDetail(int orderID){
+		ArrayList<OrderListVO> orderDetail = new ArrayList<OrderListVO>();
+		String run = "{call orderlist_detail(?, ?)}";
+
+		try {
+			Connection conn = DBConnection.getConnection();
+			CallableStatement callableStatement = conn.prepareCall(run);
+
+			callableStatement.setInt(1, orderID);
+			callableStatement.registerOutParameter(2, OracleTypes.CURSOR);
+
+			try {
+				callableStatement.execute();
+				ResultSet resultSet = (ResultSet) callableStatement.getObject(2);
+				
+				while (resultSet.next()) {
+					OrderListVO orderListVO = new OrderListVO();
+					ProductVO productVO = new ProductVO(); 
+					productVO.setProdcutId(resultSet.getInt(1));
+					productVO.setProductName(resultSet.getString(2));
+					productVO.setProductPrice(resultSet.getInt(3));
+					orderListVO.setProduct(productVO);
+					orderListVO.setOrderID(orderID);
+					orderListVO.setOrderQuantity(resultSet.getInt(4));
+					orderDetail.add(orderListVO);
+				}
+			} catch (SQLException e) {
+				System.out.println("프로시저에서 에러 발생!");
+				// System.err.format("SQL State: %s", e.getSQLState());
+				System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return orderDetail;
 	}
 }

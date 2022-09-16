75773aeeb3736c8a97849a87fdcb1f40e5c559a6
bansongs <predicthappy@naver.com>
2022-09-16 16:43:26 +0900

    문의메인페이지에서 delete실행 기능 커밋

--- tohome_thehyundai/src/com/kosa/controller/action/InquiryDeleteAction.java
+++ tohome_thehyundai/src/com/kosa/controller/action/InquiryDeleteAction.java
@@ -6,12 +6,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kosa.model.InquiryDAO;
+
 public class InquiryDeleteAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
+	    String url = "tohomeServlet?command=inquiry_list";
+	    
+	    String cseq = request.getParameter("cseq");
+	    
+	
+	      System.out.println(cseq);
+	      InquiryDAO inquiryDAO = InquiryDAO.getInstance();
+	      inquiryDAO.deleteInquiry(Integer.parseInt(cseq));
+	      System.out.println("delete 성공");
+	      request.getRequestDispatcher(url).forward(request, response);
 	}
 
 }
